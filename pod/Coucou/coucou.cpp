0d4be618f22030152687c98c5a22c65923a1c9ec
Pierre Gueth <gueth@adobe.com>
2024-01-08 06:32:41 +0100

    better init

--- pod/Coucou/coucou.cpp
+++ pod/Coucou/coucou.cpp
@@ -8,9 +8,6 @@
 #include <vector>
 #include <array>
 
-// using namespace daisy;
-// using namespace daisysp;
-
 struct OscData
 {
     uint32_t            top;
@@ -19,23 +16,14 @@ struct OscData
 
 using NoteToOscDatas = std::map<uint8_t, OscData>;
 
-NoteToOscDatas note_to_osc_datas;
-
-daisy::Parameter knob_volume;
-daisy::Parameter knob_waveform;
+NoteToOscDatas note_to_osc_datas = {};
+size_t         count_midi_clocks = 0;
+float          master_volume     = .5f;
 
 void audio_callback(daisy::AudioHandle::InputBuffer  in,
                     daisy::AudioHandle::OutputBuffer out,
                     size_t                           size)
 {
-    auto waveform = std::floor(knob_waveform.Process());
-    assert(waveform >= 0);
-    assert(waveform < 8);
-    for(auto& [note, data] : note_to_osc_datas)
-        data.osc.SetWaveform(waveform);
-
-    const auto master_volume = knob_volume.Process();
-
     using Patate = std::vector<std::complex<double>>;
 
     Patate left_channel;
@@ -69,14 +57,12 @@ void audio_callback(daisy::AudioHandle::InputBuffer  in,
     }
 }
 
-daisy::DaisyPod hardware;
-
-void midi_dump(const daisy::MidiEvent& evt)
+void midi_dump(const daisy::MidiEvent& event, daisy::DaisySeed& seed)
 {
     using MesgType = daisy::MidiMessageType;
 
     std::string label = "unknown";
-    switch(evt.type)
+    switch(event.type)
     {
         case MesgType::NoteOff: label = "note_off"; break;
         case MesgType::NoteOn: label = "note_on"; break;
@@ -91,23 +77,21 @@ void midi_dump(const daisy::MidiEvent& evt)
         default: break;
     }
 
-    hardware.seed.PrintLine("[midi] %s channel %d", label.c_str(), evt.channel);
+    seed.PrintLine("[midi] %s channel %d", label.c_str(), event.channel);
 }
 
-size_t count_midi_clocks;
-
-void midi_callback(const daisy::MidiEvent& evt,
+void midi_callback(const daisy::MidiEvent& event,
                    const float             samplerate,
                    const uint32_t          top_now)
 {
     using MesgType = daisy::MidiMessageType;
     using RTType   = daisy::SystemRealTimeType;
 
-    switch(evt.type)
+    switch(event.type)
     {
         case MesgType::SystemRealTime:
         {
-            switch(evt.srt_type)
+            switch(event.srt_type)
             {
                 case RTType::TimingClock: count_midi_clocks++; break;
                 case RTType::Stop: count_midi_clocks = 0; break;
@@ -118,7 +102,7 @@ void midi_callback(const daisy::MidiEvent& evt,
         break;
         case MesgType::NoteOn:
         {
-            const daisy::NoteOnEvent pp = evt.AsNoteOn();
+            const daisy::NoteOnEvent pp = event.AsNoteOn();
 
             auto iter_osc_data = note_to_osc_datas.find(pp.note);
             if(iter_osc_data == std::cend(note_to_osc_datas))
@@ -143,7 +127,7 @@ void midi_callback(const daisy::MidiEvent& evt,
         break;
         case MesgType::NoteOff:
         {
-            const daisy::NoteOffEvent pp = evt.AsNoteOff();
+            const daisy::NoteOffEvent pp = event.AsNoteOff();
 
             auto iter_osc_data = note_to_osc_datas.find(pp.note);
             if(iter_osc_data != std::cend(note_to_osc_datas))
@@ -152,7 +136,7 @@ void midi_callback(const daisy::MidiEvent& evt,
         break;
         case MesgType::ChannelMode:
         {
-            // const AllNotesOffEvent pp = evt.AsAllNotesOff();
+            // const AllNotesOffEvent pp = event.AsAllNotesOff();
             note_to_osc_datas.clear();
         }
         break;
@@ -164,53 +148,65 @@ int main(void)
 {
     using Curve = daisy::Parameter::Curve;
 
-    auto& hw = hardware;
+    daisy::DaisyPod pod;
+    pod.Init();
 
-    hw.Init();
-
-    hw.SetAudioBlockSize(256); // number of samples handled per callback
-    hw.SetAudioSampleRate(daisy::SaiHandle::Config::SampleRate::SAI_48KHZ);
-    hw.seed.usb_handle.Init(daisy::UsbHandle::FS_INTERNAL);
+    pod.seed.usb_handle.Init(daisy::UsbHandle::FS_INTERNAL);
+    daisy::System::Delay(100);
+    pod.seed.StartLog(true);
     daisy::System::Delay(100);
 
-    knob_volume.Init(hw.knob1, 0, 1, Curve::LINEAR);
-    knob_waveform.Init(hw.knob2, 0, 8, Curve::LINEAR);
+    pod.seed.PrintLine("[init] init");
 
-    count_midi_clocks = 0;
+    pod.SetAudioBlockSize(256); // number of samples handled per callback
+    pod.SetAudioSampleRate(daisy::SaiHandle::Config::SampleRate::SAI_48KHZ);
 
-    hw.seed.StartLog(true);
-    hw.StartAdc();
-    hw.StartAudio(audio_callback);
-    hw.midi.StartReceive();
+    daisy::Parameter knob_volume;
+    daisy::Parameter knob_waveform;
+    knob_volume.Init(pod.knob1, 0, 1, Curve::LINEAR);
+    knob_waveform.Init(pod.knob2, 0, 8, Curve::LINEAR);
 
-    hw.seed.PrintLine("[init] main loop");
+    pod.StartAdc();
+    pod.StartAudio(audio_callback);
+    pod.midi.StartReceive();
 
+    pod.seed.PrintLine("[init] main loop");
+
+    const auto audio_samplerate = pod.AudioSampleRate();
     while(true)
     {
-        const auto top_now = hw.seed.system.GetNow();
+        const auto top_now = pod.seed.system.GetNow();
 
         // controls
 
-        hw.ProcessAllControls();
+        pod.ProcessAllControls();
 
-        if(hw.button1.Pressed())
+        auto waveform = std::floor(knob_waveform.Process());
+        assert(waveform >= 0);
+        assert(waveform < 8);
+        for(auto& [note, data] : note_to_osc_datas)
+            data.osc.SetWaveform(waveform);
+
+        master_volume = knob_volume.Process();
+
+        if(pod.button1.Pressed())
             note_to_osc_datas.clear();
 
         // midi events
 
-        hw.midi.Listen();
-        while(hw.midi.HasEvents())
+        pod.midi.Listen();
+        while(pod.midi.HasEvents())
         {
-            const auto evt = hw.midi.PopEvent();
-            midi_dump(evt);
-            midi_callback(evt, hw.AudioSampleRate(), top_now);
+            const auto event = pod.midi.PopEvent();
+            midi_dump(event, pod.seed);
+            midi_callback(event, audio_samplerate, top_now);
         }
 
         // leds
 
         const auto main_clock_color = top_now % 1000 < 100 ? 1.f : 0.f;
         const auto midi_clock_color = count_midi_clocks % 24 == 0 ? 1.f : 0.f;
-        hw.led1.Set(main_clock_color, 0, midi_clock_color);
+        pod.led1.Set(main_clock_color, 0, midi_clock_color);
 
         auto note_colors = std::array<float, 3>{0, 0, 0};
         if(!note_to_osc_datas.empty())
@@ -228,10 +224,12 @@ int main(void)
             std::get<1>(note_colors) = dist(rng);
             std::get<2>(note_colors) = dist(rng);
         }
-        hw.led2.Set(std::get<0>(note_colors),
-                    std::get<1>(note_colors),
-                    std::get<2>(note_colors));
+        pod.led2.Set(std::get<0>(note_colors),
+                     std::get<1>(note_colors),
+                     std::get<2>(note_colors));
 
-        hw.UpdateLeds();
+        pod.UpdateLeds();
+
+        daisy::System::Delay(1);
     }
 }
